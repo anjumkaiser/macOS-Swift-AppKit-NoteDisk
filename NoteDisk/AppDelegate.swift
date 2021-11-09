76cb557d46590cb634b95ca194b5e06fd778fced
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2021-11-09 15:44:28 +0500

    removed redundant StatusBar code from AppDelegate

--- NoteDisk/AppDelegate.swift
+++ NoteDisk/AppDelegate.swift
@@ -10,21 +10,13 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var popover: NSPopover!
     var statusBarController: StatusBarController!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         
-        // Set the SwiftUI's ContentView to the Popover's ContentViewController
-        let popover = NSPopover()
-        popover.contentSize = NSSize(width: 360, height: 360)
-        popover.behavior = .transient
-        //popover.contentViewController = NSHostingController(rootView: contentView)
-        self.popover = popover
-        
         //Initialising the status bar
-        statusBarController = StatusBarController(popover)
+        statusBarController = StatusBarController()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
