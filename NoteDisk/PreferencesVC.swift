a950ed8f9c64f4beee926f3cd8a9617e329e4d44
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2021-11-11 16:55:56 +0500

    Defaults shown on button

--- NoteDisk/PreferencesVC.swift
+++ NoteDisk/PreferencesVC.swift
@@ -12,9 +12,14 @@ class PreferencesVC : ViewController {
     @IBOutlet weak var configureOpenNoteHotKeyButton: NSButton!
     @IBOutlet weak var configureSearchHotKeyButton: NSButton!
     
+    private let CLICK_TO_SET_MSG: String = "Click to set"
+    
     override func viewWillAppear() {
         super.viewWillAppear()
         NSApp.setActivationPolicy(.regular)
+        
+        configureOpenNoteHotKeyButton.title = Configuration.shared.openNoteHotKey ?? CLICK_TO_SET_MSG
+        configureSearchHotKeyButton.title = Configuration.shared.searchHotKey ?? CLICK_TO_SET_MSG
     }
     
     override func viewWillDisappear() {
