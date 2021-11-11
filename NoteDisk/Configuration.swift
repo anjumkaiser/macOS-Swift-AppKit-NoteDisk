d9fa9346c14910c744607eaccb90061cf007d371
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2021-11-11 15:55:09 +0500

    added Configuration singleton class

---
+++ NoteDisk/Configuration.swift
@@ -0,0 +1,18 @@
+//
+//  Configuration.swift
+//  NoteDisk
+//
+//  Created by Muhammad Anjum Kaiser on 11/11/2021.
+//
+
+import Foundation
+
+
+class Configuration {
+    
+    static var shared: Configuration = Configuration()
+    
+    private init() {
+    }
+    
+}
