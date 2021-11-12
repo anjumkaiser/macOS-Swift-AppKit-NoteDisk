226fdd333248348e3a676506882132a5851f4daf
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2021-11-12 18:38:50 +0500

    Attached keyDown() event handler to VeiwController, and trigger which hotkey to configure

--- NoteDisk/PreferencesVC.swift
+++ NoteDisk/PreferencesVC.swift
@@ -13,6 +13,14 @@ class PreferencesVC : ViewController {
     @IBOutlet weak var configureSearchHotKeyButton: NSButton!
     
     private let CLICK_TO_SET_MSG: String = "Click to set"
+    fileprivate var grabKey: KeyToGrab = .None
+    
+    override func viewDidLoad() {
+        NSEvent.addLocalMonitorForEvents(matching: .keyDown) {
+            self.keyDown(with: $0)
+            return $0
+        }
+    }
     
     override func viewWillAppear() {
         super.viewWillAppear()
@@ -27,10 +35,26 @@ class PreferencesVC : ViewController {
         NSApp.setActivationPolicy(.accessory)
     }
     
+    override func keyDown(with event: NSEvent) {
+        if grabKey == .None {
+            return
+        }
+        
+        grabKey = KeyToGrab.None
+    }
     
     @IBAction func configureOpenNoteHotKeyButton_Clicked(_ sender: Any) {
+        grabKey = .OpenNote
     }
     
     @IBAction func configureSearchHotKeyButton_clicked(_ sender: Any) {
+        grabKey = .Search
     }
+}
+
+
+fileprivate enum KeyToGrab {
+    case None
+    case OpenNote
+    case Search
 }
